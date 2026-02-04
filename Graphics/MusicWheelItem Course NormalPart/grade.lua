506f1951afe7908a52f3a3b1328d4c59a2667a29
pdaderko <pdaderko@yahoo.com>
2026-02-04 04:54:05 -0500

    Look at all scores for appropriate cleared lamp
    Loop over all scores (not just high score) for appropriate cleared icon
    Missed hold note and let go count as misses toward those icons

--- Graphics/MusicWheelItem Course NormalPart/grade.lua
+++ Graphics/MusicWheelItem Course NormalPart/grade.lua
@@ -29,60 +29,87 @@ return Def.ActorFrame{
 					assert(scorelist);
 					local scores = scorelist:GetHighScores();
 					assert(scores);
-					local topscore=0;
-					if scores[1] then
-						topscore = scores[1];
-						assert(topscore);
-						local misses = topscore:GetTapNoteScore("TapNoteScore_Miss")
-									  +topscore:GetTapNoteScore("TapNoteScore_CheckpointMiss")
-									  +topscore:GetTapNoteScore("TapNoteScore_HitMine")
-									  +topscore:GetTapNoteScore("TapNoteScore_W5")
-						local goods = topscore:GetTapNoteScore("TapNoteScore_W4")
-						local greats = topscore:GetTapNoteScore("TapNoteScore_W3")
-						local perfects = topscore:GetTapNoteScore("TapNoteScore_W2")
-						local marvelous = topscore:GetTapNoteScore("TapNoteScore_W1")
-						local hasUsedBattery = string.find(topscore:GetModifiers(),"Lives")
-						if (misses) == 0 and scores[1]:GetScore() > 0 and (marvelous+perfects)>0 then
-							if (greats+perfects) == 0 then
-								self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","MFC"))
-								self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,0.75")):effectperiod(0.1)
-							elseif greats == 0 then
-								self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","PFC"))
-								self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,0.75")):effectperiod(0.1)
-							elseif (misses+goods) == 0 then
-								self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","GreatFC"))
-								self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,0.75")):effectperiod(0.1)
-							elseif (misses) == 0 then
-								self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","GoodFC"))
-								self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,0.75")):effectperiod(0.1)
-							end;
-							self:visible(true)
-						else
-							if topscore:GetGrade() ~= 'Grade_Failed' then
-								if hasUsedBattery then
-									self:visible(true)
-									self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","Risky"))
-									self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,1")):effectperiod(1.1)
-								else
-									self:visible(true)
-									self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","LifeBar"))
-									self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,1")):effectperiod(1.1)
-								end
-							else
+					local currscore=0;
+					local bestmark=0
+					self:visible(false) --default no lamp
+					for i=1, #scores do --loop over all scores, updating the lamp corresponding to the best mark (not always highest score)
+						if scores[i] then
+							currscore = scores[i];
+							assert(currscore);
+							local misses = currscore:GetTapNoteScore("TapNoteScore_Miss")
+										  +currscore:GetTapNoteScore("TapNoteScore_CheckpointMiss")
+										  +currscore:GetTapNoteScore("TapNoteScore_HitMine")
+										  +currscore:GetTapNoteScore("TapNoteScore_W5")
+										  +currscore:GetHoldNoteScore("HoldNoteScore_MissedHold")
+										  +currscore:GetHoldNoteScore("HoldNoteScore_LetGo")
+							local goods = currscore:GetTapNoteScore("TapNoteScore_W4")
+							local greats = currscore:GetTapNoteScore("TapNoteScore_W3")
+							local perfects = currscore:GetTapNoteScore("TapNoteScore_W2")
+							local marvelous = currscore:GetTapNoteScore("TapNoteScore_W1")
+							local hasUsedBattery = string.find(currscore:GetModifiers(),"Lives")
+							if (misses) == 0 and scores[i]:GetScore() > 0 and (marvelous+perfects)>0 then
+								if (greats+perfects) == 0 then
+									if bestmark<7 then
+										bestmark=7
+										self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","MFC"))
+										self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,0.75")):effectperiod(0.1)
+									end
+								elseif greats == 0 then
+									if bestmark<6 then
+										bestmark=6
+										self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","PFC"))
+										self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,0.75")):effectperiod(0.1)
+									end
+								elseif (misses+goods) == 0 then
+									if bestmark<5 then
+										bestmark=5
+										self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","GreatFC"))
+										self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,0.75")):effectperiod(0.1)
+									end
+								elseif (misses) == 0 then
+									if bestmark<4 then
+										bestmark=4
+										self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","GoodFC"))
+										self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,0.75")):effectperiod(0.1)
+									end
+								end;
 								self:visible(true)
-								self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","Failed"))
-								self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,1")):effectperiod(1.1)
-							end
+							else
+								if currscore:GetGrade() ~= 'Grade_Failed' then
+									if hasUsedBattery then
+										if bestmark<3 then
+											bestmark=3
+											self:visible(true)
+											self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","Risky"))
+											self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,1")):effectperiod(1.1)
+										end
+									else
+										if bestmark<2 then
+											bestmark=2
+											self:visible(true)
+											self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","LifeBar"))
+											self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,1")):effectperiod(1.1)
+										end
+									end
+								else
+									if bestmark<1 then
+										bestmark=1
+										self:visible(true)
+										self:Load(THEME:GetPathG("MusicWheelItem Course NormalPart/lamp/ClearedMark","Failed"))
+										self:diffuseshift():effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,1")):effectperiod(1.1)
+									end
+								end
+							end;
+						else
+							self:visible(false)
 						end;
-					else
-						self:visible(false)
 					end;
-                else
+				else
 					self:visible(false)
 				end;
-            else
-                self:visible(false)
-            end;
-        end;
-    };
+			else
+				self:visible(false)
+			end;
+		end;
+	};
 };
